4621704198298e65c21dd4cfef5c35bf239dd05c
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-02-26 00:21:17 +0100

    tailwindCSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/main.tsx",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
