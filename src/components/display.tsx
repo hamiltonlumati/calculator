86fba8f86010ef10412a2d5f4884781d97d2385b
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-02-27 21:16:45 +0100

    display design

--- src/components/display.tsx
+++ src/components/display.tsx
@@ -1,6 +1,12 @@
 function Display(){
     return(
-        <div>
+        <div className="w-80 h-20">
+            <div className="h-10 w-80">
+
+            </div>
+            <div className="w-80 h-10">
+
+            </div>
 
         </div>
     )
