2e604b1812c2577736370b41a68696fe0c71e103
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-03-11 09:51:24 +0100

    adjusts

--- src/layout/screen.tsx
+++ src/layout/screen.tsx
@@ -9,7 +9,6 @@ interface State{
 
 type Action = {type: 'AC'} | {type: '='} | {type: '1'} | {type: '2'} | {type: '3'} | {type: '4'} | {type: '5'} | {type: '6'} | {type: '7'} | {type: '8'} | {type: '9'} | {type: '0'} | {type: '+'} | {type: '*'} | {type: '/'} | {type: '-'} | {type: '.'};
 
-
 const reducer = (state: State, action: Action): State =>{
     var operationLenght: number = state.operation.length;
     switch (action.type){
